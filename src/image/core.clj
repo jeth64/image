ab7e84746ea1b94f359b0b076772bee0ffcbc28b
jeth64 <jeth64@arcor.de>
2013-05-02 03:06:56 +0200

    Implement color space transforms: rgb2grey, rgb2hsv, hsv2grey

--- src/image/core.clj
+++ src/image/core.clj
@@ -1,21 +1,20 @@
 (ns image.core
-  (:require clojure.java.io)
-  (:import (javax.imageio ImageIO)
-           (java.awt.image BufferedImage)
-           (javax.swing JFrame)))
+  (:use image.filter)
+  (:require clojure.java.io))
 
-; (defn getPixels [^BufferedImage image] (-> image .getRaster .getDataBuffer .getData))
+(defn im2mat
+  "Takes a Java BufferedImage and returns HxWx3 matrix of RGB values"
+  [image]
+  (let [w (.getWidth image)
+        h (.getHeight image)]
+    (partition w (for [x (range w)
+                       y (range h)
+                       :let [color (java.awt.Color. (.getRGB image x y))]]
+                   [(.getRed color) (.getGreen color) (.getBlue color)]))))
 
 (defn imread [filename]
-  (javax.imageio.ImageIO/read (clojure.java.io/input-stream filename)))
-
-(def image (imread "/home/jasem/image/img/photo.jpg"))
-
-(type image)
-; (getPixels image)
-
-; (.getRGB image 0 0 (.getWidth image) (.getHeight image) java.null)
-
+  (im2mat (javax.imageio.ImageIO/read
+           (clojure.java.io/input-stream filename))))
 
 (defn view [image]
   (doto (javax.swing.JFrame. "Image")
@@ -27,75 +26,35 @@
     (.setResizable false)
     (.setVisible true)))
 
+;; colorspace transformations
 
-(view image)
+(defn rgb2grey [image]
+  (map (partial map #(quot (apply + %) (count %))) image))
 
+(defn normalize [image maximum]
+  (map (partial map (partial map #(/ %  maximum))) image))
 
-;; matrix functions
-(defn transpose [M colnum]
-  (partition colnum (apply interleave M )))
+(defn rgb2hsv-pixel [pixel]
+  (let [max (apply max pixel)
+        min (apply min pixel)
+        range (- max min)
+        argmax (.indexOf pixel max)
+        [r g b] pixel
+        h (if (= r g b) 0
+            (case argmax
+              0 (* 60 (/ (- g b) range))
+              1 (* 60 (+ 2 (/ (- b r) range)))
+              2 (* 60 (+ 4 (/ (- r g) range)))))
+        s (if (= max 0) 0 (/ range max))]
+    [h s max]))
 
+(defn rgb2hsv [image]
+  (map (partial map rgb2hsv-pixel) image))
 
-(defn conv-loop [filter v-window]
-  (apply map + (map #(map (comp (partial apply +) (partial map * (nth filter %)))
-                          (nth v-window %))
-                    (range 3))))
-
-(defn conv [im filter]; filter: ((1 2 1) (0 0 0) (-1 -2 -1))
-  (let [m (count filter)
-        n (count (first filter))
-        parted (partition m 1 (map (partial partition n 1) im))]
-    (map (partial conv-loop filter) parted)))
-
-
-(defn h-conv [im hfilter]; filter: (1 2 1)
-  (let [hsize (count hfilter)]
-    (map #(map (comp (partial apply +) (partial map * hfilter))
-               (partition hsize 1 %)) im)))
-
-(defn v-conv [im vfilter]; filter: (1 0 -1)
-  (transpose (h-conv (transpose im (count im)) vfilter) (count (first im))))
-
-(defn sep-conv [im hfilter vfilter] ; slower than conv
-  (v-conv (h-conv im hfilter) vfilter))
+(defn hsv2grey [image]
+  (map (partial map last) image))
 
 
-
-
-
-(defn rand-vec [n range]
-  (take n (repeatedly #(rand-int range))))
-
-(def im (take 4 (repeatedly #(rand-vec 5 256))))
-(def parts (partition 3 1 im))
-
-(conv im '((1 2 1) (0 0 0) (-1 -2 -1)))
-
-(-> im)
-(v-conv im [1 2 1])
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-(defn rand-mat [range dims]
-  (loop [i 0 f #(rand-int range)]
-    (if (< i (count dims))
-      (recur (inc i) #(take (nth dims i) (repeatedly f)))
-      '(f)
-      )));; not working
+;; test image
+(def im1 (imread "/home/jasem/image/img/lena.jpg"))
+(def im2 (imread "/home/jasem/image/img/photo.jpg"))
